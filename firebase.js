80b4e10ac9d8e4ee926da6150c00e810bfd6d713
ffesportsbd <wizpersonal10@gmail.com>
2026-06-30 18:20:51 +0600

    Add firebase.js
    Added Firebase configuration object with placeholders.

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// Firebase Config এখানে বসবে
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Firebase SDK পরে যোগ করা হবে।

fb3950d3566092972f6f9c62c15a0f7dcbfad692
ffesportsbd <wizpersonal10@gmail.com>
2026-06-30 18:03:21 +0600

    Add app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    alert("🔥 Welcome to FF ESPORTS BD 🔥");
+});
+
+function joinTournament() {
+    alert("Tournament Join feature coming soon!");
+}
+
+function openWallet() {
+    alert("Wallet feature coming soon!");
+}
